efd9c03a0a8b213fa5900e0211300b3a476167c1
Anderson Lima <105503214+Andersonlima13@users.noreply.github.com>
2025-08-01 20:50:40 -0300

    Update RF040.py

--- RF040.py
+++ RF040.py
@@ -1,19 +1,23 @@
-# O administrador Eduardo acessa "Calendário Acadêmico" no sistema.
+# O administrador Eduardo acessa a pagina inicial do sistema e clica em "gerenciamento"
 
-# Ele seleciona "Definir Períodos de Renovação" e vê um calendário interativo.
+# Ao clicar em gerenciamento, e exibida a opção de "calendario acadêmico"
+
+# O administrador Eduardo clica em calendario acadêmico, e é redirecionado pagina com o calendario.
+
+# Ele seleciona um botão "Definir Períodos de Renovação" , que ao clicar, abre um formulário com os campos : Periodo, Tipo, Universidade
 
 # Eduardo marca:
 
-Período de início: 01/03/2024
+Periodo : "a cada 6 meses"
 
-Período final: 15/03/2024
+Tipo: "Solicitação de documentos"
 
-Tipo: "Renovação de Documentos"
+Universidade: "UFPB"
 
 # Ao clicar em "Aplicar", o sistema pergunta:
-"Deseja enviar notificação automática aos estudantes 7 dias antes do período?"
+"Deseja enviar aplicar as regras automaticamente?"
 
 # Eduardo seleciona "Sim" e confirma.
 
 # O sistema exibe:
-*"Período configurado. 1.200 estudantes serão notificados em 22/02/2024."*
+*"Período configurado. 1.200 estudantes serão notificados 7 dias antes do inicio de um periodo"*
