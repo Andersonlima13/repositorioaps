78d56d906ccc699f247a19702cfc4ddf87df30af
Anderson Lima <105503214+Andersonlima13@users.noreply.github.com>
2025-08-01 21:57:45 -0300

    Update RF043.py

--- RF043.py
+++ RF043.py
@@ -1,7 +1,7 @@
-História do Usuário - Manter Dados dos Alunos (Agente Administrativo)
-# Carla, agente administrativa da universidade, acessa o sistema com sua matrícula e senha.
+História do Usuário - Manter Dados dos agentes administrativos (Administrador)
+# Carla como administrador acessa o sistema com sua matrícula e senha.
 
-# Ela navega até a seção "Cadastro de Alunos" e seleciona "Novo Cadastro".
+# Ela navega até a seção "Cadastro de Agente" e seleciona "Novo Cadastro".
 
 # O sistema exibe um formulário com os campos obrigatórios:
 
@@ -11,21 +11,19 @@ CPF (com máscara e validação automática)
 
 Matrícula institucional
 
-Curso (dropdown com opções cadastradas)
-
 Data de nascimento (calendário interativo)
 
 Turno (Manhã/Tarde/Noite)
 
-Foto do aluno (opcional, com pré-visualização)
+Foto do agente (opcional, com pré-visualização)
 
 # Carla preenche todos os campos e clica em "Salvar".
 
 # O sistema valida os dados e exibe a confirmação:
-✅ "Aluno cadastrado com sucesso! Nº de matrícula: 2024005678"
+✅ "Agente cadastrado com sucesso! Nº de matrícula: 2025005678"
 
 Fluxo Principal - Edição de Cadastro
-# Carla pesquisa o aluno pelo CPF na barra de busca.
+# Carla pesquisa o agente pelo CPF ou nome na barra de busca.
 
 # O sistema mostra o registro completo com botão "Editar".
 
@@ -42,15 +40,15 @@ Fluxo Alternativo 1 - Campos Obrigatórios Não Preenchidos
 
 Fluxo Alternativo 2 - CPF Já Cadastrado
 # Ao tentar cadastrar um CPF já existente na base, o sistema exibe:
-⚠️ "CPF já registrado para o aluno: João Silva (Matrícula: 2023001234). Deseja acessar o registro existente?"
+⚠️ "CPF já registrado para o aministrativo: João  (Matrícula: 2023001234). Deseja acessar o registro existente?"
 
-# Carla seleciona "Sim" e é redirecionada para o perfil do aluno.
+# Carla seleciona "Sim" e é redirecionada para o perfil do agente.
 
 Fluxo Alternativo 3 - Exclusão de Registro
-# Carla localiza o aluno e clica em "Excluir".
+# Carla localiza o agente e clica em "Excluir".
 
 # O sistema solicita confirmação:
-🗑️ *"Tem certeza que deseja excluir permanentemente o aluno Maria Oliveira (CPF: 123.456.789-00)?"*
+🗑️ *"Tem certeza que deseja excluir permanentemente o agente Luiza Oliveira (CPF: 123.456.789-00)?"*
 
 # Após confirmar, o sistema remove o registro e exibe:
 ✔️ "Registro excluído. Esta ação não pode ser desfeita."
