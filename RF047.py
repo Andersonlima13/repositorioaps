9280c9ef49ed0e298e87890ed53e6582141fe836
Anderson Lima <105503214+Andersonlima13@users.noreply.github.com>
2025-08-01 21:21:19 -0300

    Update RF047.py

--- RF047.py
+++ RF047.py
@@ -1,12 +1,14 @@
 História do Usuário - Visualizar Status do Cadastro (RF047)
+
 # A estudante Ana Clara acabou de enviar seu cadastro no UniPass pelo aplicativo.
 
 # Ela acessa o sistema e faz login com seu CPF e senha.
 
-# Na tela inicial, ela vê um card com o título:
-🔄 "Status do seu Cadastro"
+# Na tela inicial, ela clica em "Solicitações estudantis"
 
-# Ao clicar, o sistema exibe uma timeline interativa:
+# Apó isso é exibida a opção com a aba "Status de requerimento"
+
+# Ao clicar, o sistema exibe uma timeline interativa com o seguinte historico:
 
 1️⃣ Cadastro Iniciado (15/05/2024 - 10:23)
 
@@ -22,34 +24,12 @@ Declaração de vínculo (PDF)
 
 Comprovante de residência (JPG)
 
-3️⃣ Última Atualização (16/05/2024 - 14:40)
+️⃣ Última Atualização (16/05/2024 - 14:40)
 
 Status atual: "Análise em andamento"
 
 *"Previsão de conclusão: 18/05/2024"*
 
-Fluxo Principal - Acompanhamento Detalhado
-# Ana clica em "Ver detalhes" e acessa:
+# Ana clara visualiza sua situação de cadastro e volta a navegar normalmente pelo sistema
 
-Lista de documentos enviados (com ícones de formato e tamanho)
 
-Observações do administrador (ex: "Comprovante de residência precisa ser atualizado – está ilegível.")
-
-Botão "Reenviar Documentos" (se necessário)
-
-# O sistema mostra o histórico completo:
-📅 16/05 - 09:30: "Solicitação de ajuste: Comprovante de residência não está legível."
-
-Fluxos Alternativos
-1. Cadastro Aprovado
-✅ Status: "Cadastro concluído! Seu UniPass está liberado para uso."
-
-Ana recebe um e-mail e notificação push com instruções para retirar seu cartão físico.
-
-2. Cadastro Reprovado
-❌ Status: "Ajustes necessários: RG expirado. Envie nova cópia."
-
-O sistema destaca o campo problemático em vermelho e permite novo upload.
-
-3. Prazo Excedido
-⏳ Status: "Análise pendente há 7 dias. Entre em contato via: atendimento@unipass.edu.br"
